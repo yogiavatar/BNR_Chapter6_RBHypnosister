300cd049e97dfd9399dc49b604abff09de535bf4
Rashmi Bajaj <pszd8206@rbajaj.(none)>
2013-02-01 14:02:52 -0800

    Initial Commit

---
+++ RBHynosister/RBHynosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RBHynosisterAppDelegate.h
+//  RBHynosister
+//
+//  Created by Rashmi Bajaj on 2/1/13.
+//  Copyright (c) 2013 France Telecom Group (Orange) San Francisco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RBHynosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RBHynosister/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RBHynosister
+//
+//  Created by Rashmi Bajaj on 2/1/13.
+//  Copyright (c) 2013 France Telecom Group (Orange) San Francisco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RBHynosisterAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RBHynosisterAppDelegate class]));
+    }
+}
